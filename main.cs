5123a137e7cd5bf779085af9f7e82c84b422f60e
Jhoan J. Ramírez <102481805+Dojhoanrd@users.noreply.github.com>
2022-03-30 22:27:06 +0200

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string nombre;
+    Console.Write("cual es tu nombre:");
+      nombre = Console.ReadLine();
+Console.WriteLine("Hi " + nombre + " Bienvenido a mi mundo");
+      Console.ReadLine();
+ 
+  }
+}
